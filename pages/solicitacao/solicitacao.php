6fd4f9df8aa1ea6f9e2f8eb45a0f6b0ca7652d67
Guilherme <guilhermereis.gdr@gmail.com>
2025-11-26 15:47:44 -0300

    ...

--- pages/solicitacao/solicitacao.php
+++ pages/solicitacao/solicitacao.php
@@ -13,7 +13,7 @@ $usuario = Store::get('usuario');
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ALOCATEC - Solicitações</title>
+  <title>ALOCATEC - Solicitações 1</title>
   <link rel="stylesheet" href="solicitacao.css">
   <link rel="icon" href="img/logo.png">
   <link rel="shortcut icon" href="img/logo.png">
